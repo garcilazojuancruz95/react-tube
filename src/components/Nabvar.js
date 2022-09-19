52363f041d1e4654c6c15e1356673240a7eeb4e4
Juan Cruz <garcilazo.juancruz@hotmail.com>
2022-09-19 13:43:01 -0300

    primer commit

---
+++ src/components/Nabvar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    
+    render() {
+        return (
+            <header>
+                <strong><em>React-Tube-App</em></strong>
+            </header>
+        );
+    }
+}
+
+export default Navbar;
